5d5b53d991d6c7e1734480438fc316fb099aa269
Ilya <ikandrushin@list.ru>
2024-10-18 14:04:19 +0300

    Added an assembly server.cpp in CMakeLists.txt and corrected the errors in server.cpp

--- src/server.cpp
+++ src/server.cpp
@@ -10,12 +10,13 @@ using namespace std;
 
 int main()
 {
-    const char IP_SERV[] = "";
-    const int PORT_NUM = 0;
+    const char IP_SERV[] = "127.0.0.1";
+    const int PORT_NUM = 1234;
     const short BUFF_SIZE = 1024;
 
     int erStat;
 
+    // IP in string format to numeric format
     in_addr ip_to_num;
     erStat = inet_pton(AF_INET, IP_SERV, &ip_to_num);
 
@@ -28,9 +29,10 @@ int main()
         return 1;
     }
 
+    // WinSock initialization
     WSADATA wsData;
 
-    int erStat = WSAStartup(MAKEWORD(2, 2), &wsData);
+    erStat = WSAStartup(MAKEWORD(2, 2), &wsData);
 
     if (erStat != 0) {
 
@@ -42,6 +44,7 @@ int main()
         cout << "WinSock initialization is OK" << endl;
     }
 
+    // Server socket initialization
     SOCKET ServSock = socket(AF_INET, SOCK_STREAM, 0);
 
     if (ServSock == INVALID_SOCKET) {
@@ -58,6 +61,7 @@ int main()
         cout << "Server socket initialization is OK\n";
     }
 
+    // Server socket binding
     sockaddr_in servInfo;
     ZeroMemory(&servInfo, sizeof(servInfo));
 
@@ -81,6 +85,7 @@ int main()
         cout << "Binding socket to server info is OK" << endl;
     }
 
+    // Starting to listen to any Clients
     erStat = listen(ServSock, SOMAXCONN);
 
     if (erStat != 0) {
@@ -98,6 +103,7 @@ int main()
         cout << "Listening ..." << endl;
     }
 
+    // Client socket creation and acception in case of connection
     sockaddr_in clientInfo;
     ZeroMemory(&clientInfo, sizeof(clientInfo));
 
@@ -128,6 +134,7 @@ int main()
         << clientIP << endl;
     }
 
+    // Exchange text data between Server and Client
     vector<char> servBuff(BUFF_SIZE), clientBuff(BUFF_SIZE);
     short packet_size = 0;
 
@@ -139,6 +146,7 @@ int main()
         cout << "Your (host) message: ";
         fgets(clientBuff.data(), clientBuff.size(), stdin);
 
+        // Disconnection if client send "xxx"
         if (clientBuff[0] == 'x' && clientBuff[1] == 'x' && clientBuff[2] == 'x') {
 
             shutdown(ClientConn, SD_BOTH);
